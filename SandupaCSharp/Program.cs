ae34ea3d3adec4cdc8a81a02cd300e6e5eafa40b
SandupaTech <93241017+SandupaTech@users.noreply.github.com>
2021-11-30 10:59:34 +0530

    #

--- SandupaCSharp/Program.cs
+++ SandupaCSharp/Program.cs
@@ -10,7 +10,13 @@ namespace SandupaCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string CharacterName = "Chris";
+            int CharacterAge = 40;
+
+            Console.WriteLine("There once was a man named " + CharacterName);
+            Console.WriteLine("He was " + CharacterAge + " years old");
+            Console.WriteLine("He Really Liked name " + CharacterName);
+            Console.WriteLine("But didn't like being " + CharacterAge);
 
             Console.ReadLine();
         }
